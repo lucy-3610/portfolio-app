b49b4a75e8e5a96d82a69d30e6b0078518fe266b
Lucy Swett <lucy.swett@alum.lehigh.edu>
2023-08-25 14:36:49 -0400

    improved responsiveness

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -2,24 +2,27 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from './components/Header';
 
+import lp_img from './Portfolio-Images/Lucy Swett Portfolio Landing Page BG.png';
+
 class LandingPage extends React.Component {
     render() {
         return (
             <div className="landing-page">
-                <div className="lp-bg" style={{ opacity: "60%" }}></div>
                 <Header />
+                <div className="lp-bg" style={{ opacity: "60%" }}></div>
 
-                <div className="container">
+                <div className="container z-1">
                     <div className="row">
                         <div className="col-auto">
                             <h2>Hi, I'm</h2>
                             <h1>Lucy Swett</h1>
                             <h3>Web Developer</h3>
-                            <p>Motivated young professional seeking to empower people through technology.</p>
+                            <p>Motivated young professional seeking to<br className="d-none d-lg-block d-xl-none" /> empower people through technology.</p>
                             <Link className="btn-theme" to="/portfolio">PORTFOLIO</Link>
                         </div>
                     </div>
                 </div>
+                <img className="lp-img d-block d-lg-none" src={lp_img} />
             </div>
         );
 
