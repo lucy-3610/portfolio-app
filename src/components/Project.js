85f143eeab5c65b57d783632967a011462588201
Lucy Swett <lucy.swett@alum.lehigh.edu>
2023-09-14 14:19:25 -0400

    updated apple icon and portfolio links to external link

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 
 class Project extends React.Component {
     render() {
-        const {path, image, title} = this.props;
+        const {url, path, image, title} = this.props;
         return (
             <div className="col-md-6 col-lg-4 project">
-                {/* <a target="_blank" rel="noopener noreferrer" href={url}></a> */}
+                <a target="_blank" rel="noopener noreferrer" href={url}>
                 {/* <Link className="btn-theme px-3" to="/contact">Let's Talk</Link> */}
-                <a href={`/portfolio-app/${path}`}>
+                {/* <a href={`/portfolio-app/${path}`}> */}
                     <img className="project-img" alt={image} src={image} />
                     <div className="overlay">
                         <div className="text font-weight-bold">{title}</div>
